bc985c382eea02d558b3e70dc71377de94e379e4
LARRYDDEV <wajuore456@gmail.com>
2022-03-29 14:01:30 +0100

    crypto-app

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,25 +5,25 @@ function Navbar(params) {
    return(
       <section className="nav-sec">
          <div>
-            <nav className="navbar navbar-expand fixed-top-lg navbar-light bg-light">
-               <div className="container-fluid">
-                  <Link className="navbar-brand a-brand" to="/">CRYPTO</Link>
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                     <span className="navbar-toggler-icon"></span>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               <div class="container-fluid">
+                  <Link class="navbar-brand" to="/">CRYPTO</Link>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
                   </button>
-                  <div className="collapse navbar-collapse ul-nav" id="navbarNav">
-                     <ul className="navbar-nav">
-                        <li className="nav-item">
-                           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
-                           <a className="nav-link active" href="#">Features</a>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
                         </li>
-                        <li className="nav-item">
-                           <a className="nav-link active" href="#">Pricing</a>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">Pricing</a>
                         </li>
-                        <li className="nav-item">
-                           <a className="nav-link active">Disabled</a>
+                        <li class="nav-item">
+                           <a class="nav-link disabled">Disabled</a>
                         </li>
                      </ul>
                   </div>
